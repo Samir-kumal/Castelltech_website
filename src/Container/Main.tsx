988e31f23edd900846cfb0a0a2b0571aa3f43d01
Samir-kumal <kumalsameer124@gmail.com>
2023-09-14 15:49:46 +0545

    changes

--- src/Container/Main.tsx
+++ src/Container/Main.tsx
@@ -17,7 +17,7 @@ import { CONFETTI_DARK, CONFETTI_LIGHT } from "../lib/Theme";
 export default function Main() {
   return (
     <Container
-      height={{ base:"6xl",lg:"2xl"}}
+      height={{ base:"4xl",lg:"2xl"}}
       // marginBottom={{base:"2", lg:"0"}}
       maxW={"7xl"}
       marginTop={"1"}
